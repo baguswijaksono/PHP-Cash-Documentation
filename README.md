e11fbe8821f11c52433fe0f9deaaff200307e130
baguswijaksono <baguswijaksono292@gmail.com>
2022-12-03 19:23:28 +0700

    tambah banner payment method

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Project Final Pemrograman Web Dinamis
+
+/## Screenshot 
+
+| Payment 1 | Payment 2 | Payment 3 | Payment 4 |
+|:---------------:|:----------------:|:------------------:|:------------------:|
+| <img width="300" alt="2 columns" src="https://user-images.githubusercontent.com/106671990/205439385-80b24143-fefa-418e-8d34-4ee32f5e5a0e.png"> | <img width="300" alt="3 columns" src="https://user-images.githubusercontent.com/106671990/205439321-c3f9bfec-9f25-4914-95f4-c068b58583f4.png"> | <img width="280" alt="4 columns" src="https://user-images.githubusercontent.com/106671990/205439342-b2f15f4e-c303-4d11-b290-6a0af704dbb6.png"> | <img width="280" alt="4 columns" src="https://user-images.githubusercontent.com/106671990/205439361-784d54bb-0653-49f4-b091-94548171a5e0.png"> |
+
--- assets/php/payment_method.php
+++ assets/php/payment_method.php
@@ -9,8 +9,13 @@
                 "https://user-images.githubusercontent.com/106671990/205439342-b2f15f4e-c303-4d11-b290-6a0af704dbb6.png",
                 "https://user-images.githubusercontent.com/106671990/205439361-784d54bb-0653-49f4-b091-94548171a5e0.png");
 
+    $path = array("https://user-images.githubusercontent.com/106671990/205439385-80b24143-fefa-418e-8d34-4ee32f5e5a0e.png",
+                "https://user-images.githubusercontent.com/106671990/205439321-c3f9bfec-9f25-4914-95f4-c068b58583f4.png",
+                "https://user-images.githubusercontent.com/106671990/205439342-b2f15f4e-c303-4d11-b290-6a0af704dbb6.png",
+                "https://user-images.githubusercontent.com/106671990/205439361-784d54bb-0653-49f4-b091-94548171a5e0.png");
+
     for ($x = 0; $x <= 3; $x++) {
-        echo "<a class='card' href='#'>";
+        echo "<a class='card' href='$path[$x]'>";
         echo "<div class='card__background' style='background-image: url($py[$x])'></div>";
         echo "</a>";
       } 
