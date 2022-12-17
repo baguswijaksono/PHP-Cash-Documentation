17a47556bdcb837a7e34aa6d8eb4ae6047358736
baguswijaksono <baguswijaksono292@gmail.com>
2022-12-17 09:05:47 +0700

    baru

--- assets/php/admin.php
+++ assets/php/admin.php
@@ -1,12 +1,31 @@
-<!DOCTYPE html>
+<?php
+    session_start();
+    $username = $_SESSION['username'];
+    include "config.php";
+    $db = new Database();
+
+    foreach($db->login_admin($username) as $x){
+        $akses_id = $x['akses_id'];
+        if($akses_id=='1'){
+?>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>halaman admin</h1>
-</body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  </head>
+  <body>
+            <?php
+                include "dashboard.php";
+            ?>
+  </body>
 </html>
+<?php
+        }
+        else{
+            echo "Anda belum login";
+            header("Location: login.php");
+        }
+    }
+?>
